e7a09e8ebe59c63da2d16cc23c4427a7c7b8c0f8
Tim Sullivan <timothyj.sullivan1@gmail.com>
2022-05-30 21:48:13 -0500

    moved functions out of main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 # import io
 # import json
 import os
-from datetime import datetime
 
-from flask import render_template
 
 #  Flask, render_template, request, send_file
 import fastapi
 from fastapi.staticfiles import StaticFiles
+from api import invoice_api
 from views import home
 import uvicorn
 
@@ -26,45 +25,7 @@ def configure():
 def configure_routing():
     app.mount("/static", StaticFiles(directory="static"), name="static")
     app.include_router(home.router)
-
-
-# Get current date
-def get_current_date():
-    return datetime.now().strftime("%B %-d, %Y")
-
-
-# Get default data from file
-def get_default_data_from_file(filepath):
-    with open(filepath, "r") as f:
-        data = f.read()
-    return data
-
-
-# Generate data to submit to invoice based off of posted data or default data
-def generate_data(posted_data, data):
-    if posted_data:
-        data["invoice_number"] = posted_data["invoice_number"]
-        data["from_addr"] = posted_data["from_addr"]
-        data["to_addr"] = posted_data["to_addr"]
-        data["due_date"] = posted_data["due_date"]
-        data["items"] = posted_data["items"]
-
-    data["total"] = round(sum([i["charge"] for i in data["items"]]), 2)
-    return data
-
-
-# Render template from my data
-def render_template_from_data(data, today):
-    return render_template(
-        "invoice.html",
-        date=today,
-        from_addr=data.get("from_addr"),
-        to_addr=data.get("to_addr"),
-        items=data.get("items"),
-        total=data.get("total"),
-        invoice_number=data.get("invoice_number"),
-        due_date=data.get("due_date"),
-    )
+    app.include_router(invoice_api.router)
 
 
 # @app.route("/", methods=["GET", "POST"])
